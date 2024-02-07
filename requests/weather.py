55e9276b8a9e8a2c975d4831962b2c9b041ee8aa
evgeni <92616bdb@mail.ru>
2024-02-07 15:25:57 +0300

    new

---
+++ requests/weather.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'https://api.openweathermap.org/data/2.5/weather'
+api_token = '8e2b54bec35e94b0562bc4972a76f3c4'
+params = {'q': 'Minsk', 'appid': api_token}
+
+response = requests.get(url, params=params)
+print(response.status_code)
+print(response.headers)
+result_json = response.json()
+
+print(result_json)
+print(result_json['name'])
+print(result_json['sys']['country'])
+print(result_json['main']['temp_min'])
+print(result_json['main']['temp_max'])
